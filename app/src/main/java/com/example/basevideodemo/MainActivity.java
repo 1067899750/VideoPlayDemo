a41aa4edc309f97e624cdae4824ae9a9c2bf335f
puyantao <puyantao123759>
2020-09-21 16:44:05 +0800

    #

--- app/src/main/java/com/example/basevideodemo/MainActivity.java
+++ app/src/main/java/com/example/basevideodemo/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MyTabActivity.startMyTabActivity(MainActivity.this);
+                VideoAndMusicActivity.startVideoAndMusicActivity(MainActivity.this);
             }
         });
     }
